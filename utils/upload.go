0bf0723700980ff1e587399f8109c149bd13e5c0
Elvin Ega <proega3@gmail.com>
2025-05-24 08:01:53 +0300

    Add file upload utility for saving multipart files

---
+++ utils/upload.go
@@ -0,0 +1,39 @@
+package services
+
+import (
+	"fmt"
+	"mime/multipart"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func SaveFile(files []*multipart.FileHeader) (string, error) {
+	if len(files) == 0 {
+		return "", nil
+	}
+	file := files[0]
+	uploadPath := "uploads/" + fmt.Sprintf("%d_%s", time.Now().UnixNano(), filepath.Base(file.Filename))
+	if err := os.MkdirAll("uploads", os.ModePerm); err != nil {
+		return "", err
+	}
+	if err := saveUploadedFile(file, uploadPath); err != nil {
+		return "", err
+	}
+	return "/" + uploadPath, nil
+}
+
+func saveUploadedFile(file *multipart.FileHeader, dst string) error {
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	_, err = out.ReadFrom(src)
+	return err
+}
