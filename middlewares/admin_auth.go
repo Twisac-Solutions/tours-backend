5f5a28d2d47abcb1573b004dcf109ee9701fce1f
Elvin Ega <proega3@gmail.com>
2025-06-25 19:10:42 +0300

    Update admin_auth.go

--- middlewares/admin_auth.go
+++ middlewares/admin_auth.go
@@ -1,18 +1,21 @@
 package middlewares
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func AdminOnly(c *fiber.Ctx) error {
 	// Skip auth check for login route
+	log.Println("Checking admin auth: ", c.Path())
 	if c.Path() == "/admin/login" {
 		return c.Next()
 	}
 
 	// Auth check for all other admin routes
 	role := c.Locals("userRole")
-	if role != "admin" {
+	if role != "admin" && role != "superadmin" {
 		return c.Status(403).JSON(fiber.Map{"error": "Forbidden"})
 	}
 	return c.Next()
