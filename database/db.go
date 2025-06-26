2ebce3cc96fd352e6e0a27435a9b84b70e17a514
Elvin Ega <proega3@gmail.com>
2025-06-26 18:39:12 +0300

    Update DB connection and add Category to auto-migrate

--- database/db.go
+++ database/db.go
@@ -12,10 +12,25 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	var err error
-	DB, err = gorm.Open(sqlite.Open("tout.db"), &gorm.Config{})
+	DB, err = gorm.Open(sqlite.Open("tour.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database!")
 	}
 
-	DB.AutoMigrate(&models.User{})
+	DB.AutoMigrate(&models.User{}, &models.Category{})
+	// DB.AutoMigrate(
+	// 		&models.User{},
+	// 		&models.Admin{},
+	// 		&models.Category{},
+	// 		&models.Destination{},
+	// 		&models.Event{},
+	// 		&models.Media{},
+	// 		&models.Review{},
+	// 		&models.Tag{},
+	// 		&models.Tour{},
+	// 	)
+	if err != nil {
+		log.Fatal("Failed to auto-migrate:", err)
+	}
+	log.Println("Auto-migration completed successfully")
 }
