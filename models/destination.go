a6919bada9c4b4fb2e22798f49218919776e8e06
Elvin Ega <proega3@gmail.com>
2025-06-26 19:10:29 +0300

    Refactor Destination model and add BeforeCreate hook

--- models/destination.go
+++ models/destination.go
@@ -4,20 +4,27 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type Destination struct {
 	ID          uuid.UUID `gorm:"type:text;primaryKey" json:"id"`
 	Name        string    `json:"name"`
-	Slug        string    `gorm:"uniqueIndex" json:"slug"`
 	Country     string    `json:"country"`
 	Region      string    `json:"region"`
 	Description string    `json:"description"`
-	CoverImage  Media     `gorm:"embedded" json:"coverImage"`
-	Gallery     []string  `gorm:"type:text[]" json:"gallery"`
-	Tours       []string  `gorm:"type:text[]" json:"tours"`
-	Events      []string  `gorm:"type:text[]" json:"events"`
-	CreatedBy   uuid.UUID `json:"createdBy"`
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt"`
+	CoverImage  Media     `gorm:"foreignKey:DestinationID" json:"coverImage"`
+	// Gallery     []string  `gorm:"type:text[]" json:"gallery"`
+	// Tours     []string  `gorm:"type:text[]" json:"tours"`
+	// Events    []string  `gorm:"type:text[]" json:"events"`
+	CreatedBy uuid.UUID `json:"createdBy"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+func (m *Destination) BeforeCreate(tx *gorm.DB) (err error) {
+	if m.ID == uuid.Nil {
+		m.ID = uuid.New()
+	}
+	return
 }
